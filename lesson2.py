fb96e80af5e8db26d08faa325738ac8a7cddfcc5
unknown <sattytadevossian@gmail.com>
2024-03-04 20:21:31 +0400

    add files

---
+++ lesson2.py
@@ -0,0 +1,20 @@
+# # inputs=[1.2,5.1,2.1]
+# # weights =[3.1,2.1,8.7]
+# # bias = 3.0
+# # output = inputs[0]+weights[0]+inputs[1]
+# # print(output)
+
+# # inputs=[1.0,2.0,3.0,2.5]
+# # weights1=[0.2,0.8,-0.5,1.0]
+# # weights2=[0.5,-0.91,0.26,-0.5]
+# # weights3=[-0.26,-0.27,0.17,0.87]
+# # #lossfunctions
+
+# # bias1=2.0
+# # bias2=3.0
+# # bias3=0.5
+
+# # output1=[inputs[0]*weights1[0]+inputs[1]*weights1[1]+inputs[2]*weights1[2]+inputs[3]*weights1[3]+bias1]
+
+# # print(output1)
+# import numpy as np
